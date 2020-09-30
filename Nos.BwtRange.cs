1d174038f08371b8f78c416a3cc19bd129b4281b
aditi1243 <69077135+aditi1243@users.noreply.github.com>
2020-09-30 21:58:09 +0530

    Add files via upload

---
+++ Nos.BwtRange.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersBetweenrange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i;
+            Console.Write("\n\n");
+            Console.Write("Display the numbers between 100 to 200:\n");
+            Console.Write("---------------------------------------");
+            Console.Write("\n\n");
+
+            //Console.WriteLine("The first 10 natural number are:");
+
+            for (i = 101; i < 200; i++)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.Write("\n\n");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}

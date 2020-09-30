97c97d37f2db7773c823888d6af956f2f2268f75
aditi1243 <69077135+aditi1243@users.noreply.github.com>
2020-09-30 21:49:49 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenNaturalNos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 0;
+
+            Console.WriteLine("Even Numbers :");
+            for (i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            Console.WriteLine("\nOdd Numbers :");
+            for (i = 1; i <= 10; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
+
+
+        }
+    }
+}

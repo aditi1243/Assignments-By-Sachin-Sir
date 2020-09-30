f57722f69d9e9c2564dc184d6f5a74a9767ff64a
aditi1243 <69077135+aditi1243@users.noreply.github.com>
2020-09-30 21:57:12 +0530

    Add files via upload

---
+++ First(10)naturalNo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_10_NaturalReverseOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=10, num;
+
+            //Read a number from user
+            //Console.Write("Enter number: ");
+            //num = Convert.ToInt32(Console.ReadLine());
+
+            ///*Running loop from the number entered by user,
+              //and Decrementing by 1*/
+            for (i = 10; i >= 1; i--)
+            {
+                Console.WriteLine("\n" + i);
+            }
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}

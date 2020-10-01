24fb0f20bbdfe244fc3a3f1e903da4bae37fae08
aditi1243 <69077135+aditi1243@users.noreply.github.com>
2020-10-01 20:14:22 +0530

    Add files via upload

---
+++ 6-(Prime-2-17).cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6__Prime_2_17_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPrime = true;
+            Console.WriteLine("Prime Numbers : ");
+            for (int i = 2; i <= 17; i++)
+            {
+                for (int j = 2; j <= 17; j++)
+                {
+
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+
+                }
+                if (isPrime)
+                {
+                    Console.Write("\t" + i);
+                }
+                isPrime = true;
+            }
+            Console.ReadLine();
+        }
+           
+
+        }
+    }
+

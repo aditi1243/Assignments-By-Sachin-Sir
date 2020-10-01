36ef80f13c4bfc0606f4061317731418ded430aa
aditi1243 <69077135+aditi1243@users.noreply.github.com>
2020-10-01 20:17:37 +0530

    Add files via upload

---
+++ 9-Printing-Pattern.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9__pattern_printing_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, space = 1;
+            for (i = 6; i >= 1; i--)
+            {
+                for (j = 1; j <= space; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                for (j = i -1; j >= 1; j--)
+                {
+                    Console.Write(j);
+                }
+                Console.Write("\n");
+                space++;
+            }
+            Console.ReadLine();
+        }
+    }
+}

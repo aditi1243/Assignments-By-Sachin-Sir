63c62d5ab6974b0df59ad3df4d99e24d0d8d300a
aditi1243 <69077135+aditi1243@users.noreply.github.com>
2020-10-01 20:12:10 +0530

    Add files via upload

---
+++ 7-Pattern-Prints.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Pattern_Prints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, k;
+
+            for (i = 5; i >= 1; i--)
+            {
+                for (j = 1; j <= i; ++j)
+                    Console.Write(j);
+
+                for (k = 5 - i; k >= 1; k--)
+                    Console.Write(" ");
+
+                Console.Write("\n");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

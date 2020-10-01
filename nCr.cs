fea9aefdbc15aadd94f0f6c3ad5a5d4aa5b64eaa
aditi1243 <69077135+aditi1243@users.noreply.github.com>
2020-10-01 20:03:23 +0530

    Add files via upload

---
+++ nCr.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nCr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, r, ncr;
+            Console.WriteLine("enter the value of n");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the value of r");
+            r = Convert.ToInt32(Console.ReadLine());
+            ncr = (fact(n) / (fact(n - r) * fact(r)));
+            Console.WriteLine("the output: {0}", ncr);
+            Console.ReadLine();
+        }
+
+        static int fact(int num)
+        {
+            int i = 1, fact = 1;
+
+            for (i = 1; i <= num; i++)
+            {
+                fact = fact * i;
+            }
+
+            return fact;
+        }
+    }
+}

36cac8653059df811a18b7cf14dd31ac140b7e52
aditi1243 <69077135+aditi1243@users.noreply.github.com>
2020-10-01 20:13:31 +0530

    Add files via upload

---
+++ 6-series.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n,d;
+            Console.WriteLine("Enter the value of n :");
+            n = Convert.ToInt32(Console.ReadLine());
+            for(i=1;i<=n;i++)
+            {
+                d = i * i * i;
+                Console.WriteLine("{0}",d);
+                Console.WriteLine(" \n ");
+            }
+            Console.ReadKey();
+        }
+    }
+}

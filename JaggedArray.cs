43a31a90bbdaa9a0901370f2633eb244a209d0f1
aditi1243 <69077135+aditi1243@users.noreply.github.com>
2020-10-08 21:34:38 +0530

    Update JaggedArray.cs

--- JaggedArray.cs
+++ JaggedArray.cs
@@ -12,30 +12,42 @@ namespace Jagged_Array
         {
             Console.WriteLine("Hello world");
             //its demonstration of 2-d array//the jagged array will be of 2-d array
-            int[][] arr = new int[2][];//declared the array -Multi dimensional
+           // int[][] arr = new int[2][];//declared the array -Multi dimensional
 
             //making the above array into jagged
-            arr[0] = new int[3];//initializing the value to the jagged array
-            arr[1] = new int[5];
+            //arr[0] = new int[3];//initializing the value to the jagged array
+            //arr[1] = new int[5];
 
-            int[][] Jaggedarray = new int[5][];
-            Jaggedarray[0] = new int[3];
-            Jaggedarray[1] = new int[5];
-            Jaggedarray[2] = new int[7];
-            Jaggedarray[3] = new int[9];
-            Jaggedarray[4] = new int[11];
+           // int[][] Jaggedarray = new int[4][];
+            //Jaggedarray[0] = new int[3];
+            //Jaggedarray[1] = new int[5];
+            //Jaggedarray[2] = new int[7];
+            //Jaggedarray[3] = new int[9];
+            //Jaggedarray[4] = new int[11];
 
             //another way of assigning the values to jagged array
-
-            Jaggedarray[0] = new int[] { 3, 5, 7, 9 };
-            Jaggedarray[1] = new int[] { 0, 1, 2, 3, 4, 5, 6, 7 };
-            Jaggedarray[2] = new int[] { 1, 0, 4, 5, 8, 9, 6, 5, 4 };
+             
+            int[][] jagarr = new int[4][];
+            Jaggedarray[0] = new int[]{2,4}
+            Jaggedarray[1] = new int[] { 3, 5, 7, 9 };
+            Jaggedarray[2] = new int[] { 0, 1, 2, 3, 4, 5, 6, 7 };
+            Jaggedarray[3] = new int[] { 1, 0, 4, 5, 8, 9, 6, 5, 4 };
 
             //Shorthand of jagged Array
-            int[][] Jaggedarray1 =
-            {new int[]{1,2,3,4,5 },
-            new int[]{2,4,6,8,2, }
-            Console.ReadKey();
+            //int[][] Jaggedarray1 =
+            //{new int[]{1,2,3,4,5 },
+            //new int[]{2,4,6,8,2, }
+             for(int i=0;i<jagarr.Length;i++)
+            {
+                Console.WriteLine("Elements of ({0}):", i);
+                for (int j=0; j < jagarr[i].Length; j++)
+                {
+                    Console.WriteLine(jagarr[i][j]);
+                }
+
+            }
+            
+            //Console.ReadKey();
         }
             }
         }
